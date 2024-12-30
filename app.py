68e815c6e36b5c727e7e12ecf6969f114b56628e
Jingjing Wu <wuj76@cardiff.ac.uk>
2024-12-30 00:56:43 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from digital_portfolio import app
+from digital_portfolio import app, db
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080, debug=True)
